10814d4aff3930b0ecea3dca04ef56f5faeb90fe
zhangge <zhangge@zhongan.com>
2018-04-19 17:36:53 +0800

    add props components

--- app/app.js
+++ app/app.js
@@ -2,11 +2,17 @@ import React from 'react'
 import ReactDom from 'react-dom'
 
 import Hello from './components/Hello'
+import CompProp from './components/Prop'
 
 const Element = ()=>{
-    return <h1>Hello, world !!!</h1>
+    return (
+        <div>
+            <h1>Hello, world </h1>
+            <Hello />
+            <CompProp name="hahahha" />
+        </div>
+    )
 }
-
 
 ReactDom.render(
     <Element />,
---
+++ app/components/Prop.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const compProp = (props) => {
+    return (
+        <h2>hello. props = {props.name}</h2>
+    )
+}
+
+export default compProp
