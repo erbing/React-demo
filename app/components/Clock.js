3150a4182c970c6eacb1e44afa96cfc99821a494
zhangge <zhangge@zhongan.com>
2018-04-20 18:16:46 +0800

    change  clock

--- app/components/Clock.js
+++ app/components/Clock.js
@@ -3,14 +3,14 @@ import React from 'react'
 class Clock extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {date: new Date()}
+        this.state = {date: 'this is state'}
     }
 
     render() {
         return (
             <div>
                 <h2>this is clock components</h2>
-                <h3> It's { 222 } </h3>
+                <h3> It's {this.state.date} </h3>
             </div>
         )
     }
