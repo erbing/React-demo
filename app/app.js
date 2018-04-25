5c316bf5b35b9e95b80c7e507248f7d8c18f62c1
zhangge <zhangge@zhongan.com>
2018-04-25 18:59:43 +0800

    add rputer

--- app/app.js
+++ app/app.js
@@ -1,25 +1,33 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 
-import Hello from './components/Hello'
-import CompProp from './components/Prop'
-import Clock from './components/Clock'
-import OnClick from './components/OnClick'
-import IfRender from './components/IfRender'
-import SanMu from './components/SanMu'
-import StopRenderCpt from './components/StopRenderCpt'
-import List from './components/List'
-import Form from './components/Form'
+// import Hello from './components/Hello'
+// import CompProp from './components/Prop'
+// import Clock from './components/Clock'
+// import OnClick from './components/OnClick'
+// import IfRender from './components/IfRender'
+// import SanMu from './components/SanMu'
+// import StopRenderCpt from './components/StopRenderCpt'
+// import List from './components/List'
+// import Form from './components/Form'
 
-import Tisheng from './components/Tisheng'
-import SplitPane from './components/SplitPane'
-import PropsChildren from './components/PropsChildren'
+// import Tisheng from './components/Tisheng'
+// import SplitPane from './components/SplitPane'
+// import PropsChildren from './components/PropsChildren'
+
+const APP = () => (
+    <div>
+        <Header />
+        <Main />
+    </div>
+)
 
 const Element = ()=>{
     return (
         <div>
             <h1>Hello, world </h1>
-            <Hello />
+            {/* <Hello /> */}
             {/* <CompProp name="hahahha" age="123123"/>
             <Clock time="2000"/>
             <Clock time="4000"/>
@@ -30,14 +38,15 @@ const Element = ()=>{
             <StopRenderCpt />
             <List />
             <Form /> */}
-            <Tisheng />
+            {/* <Tisheng />
             <SplitPane />
-            <PropsChildren />
+            <PropsChildren /> */}
         </div>
     )
 }
 
-ReactDom.render(
-    <Element />,
-    document.getElementById('apps')
-)
+ReactDom.render((
+    <BrowserRouter>
+        <APP />
+    </BrowserRouter>
+), document.getElementById('apps'))
---
+++ app/common/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default class Header extends React.Component {
+    constructor(props) {
+        super(pros)
+    }
+
+    render() {
+        return (
+            <div>
+                <ul>
+                    <li></li>
+                </ul>
+            </div>
+        )
+    }
+}   
