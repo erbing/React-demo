81c20b7d814e2ca2e00f48d35562f715759074f6
SmallW <447587073@qq.com>
2018-04-28 16:06:48 +0800

    add some css

--- app/common/Main.js
+++ app/common/Main.js
@@ -2,28 +2,29 @@ import React from 'react'
 import { Switch, Route, Link } from 'react-router-dom'
 
 import style from '../assets/less/index.less'
+import styleMain from '../assets/less/main.less'
 
 const Home = (props) => (
-    <div>
+    <div className="router">
         this is home pages
     </div>
 )
 
 const Roster = (props) => (
-    <div>
+    <div className="router">
         this is index pages
         <li> <Link to={'/msg/123'}> msg </Link></li>
     </div>
 )
 
 const Schedule = (props) => (
-    <div>
+    <div className="router">
         this is my pages
     </div>
 )
 
 const Number = (props) => (
-    <div>
+    <div className="router">
         this is Number pages and this is my prama id : {props.match.params.id}
     </div>
 )
